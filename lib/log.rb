fe084cac8d76f22d5f93006bdd31bcd58a846d3b
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-18 14:38:56 +0100

    Implement log history with date only

--- lib/log.rb
+++ lib/log.rb
@@ -1,2 +1,15 @@
+require 'date'
+
 class Log
+
+  attr_reader :history
+
+  def initialize
+    @history = []
+  end
+
+  def store_date(date)
+    @history << date
+  end
+
 end
--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -1,7 +1,10 @@
 describe Log do
+  subject(:log) { described_class.new }
 
-  it "responds to new" do
-    expect(Log).to respond_to(:new)
+  it "stores the date when a deposit is made" do
+    date = Date.new
+    log.store_date(Date.new)
+    expect(log.history).to eq([date])
   end
 
 end
